9eed3ad8af6a28ded7a017a2e8a2b225fc758d12
galilea-g19 <galileagranados19@gmail.com>
2026-04-27 18:48:03 -0600

    fix: cli libraries and time format

--- src/timer.ts
+++ src/timer.ts
@@ -1,13 +1,24 @@
+function formatTime(seconds: number) {
+  const minutes = Math.floor(seconds / 60);
+  const restSeconds = seconds % 60;
+
+  const minText = String(minutes).padStart(2, "0");
+  const segText = String(restSeconds).padStart(2, "0");
+
+  return `${minText}:${segText}`;
+}
+
 export function startTimer(seconds: number) {
   let remaining = seconds;
 
   const interval = setInterval(() => {
     remaining--;
-    console.log(`⏱️ Tiempo restante: ${remaining} segundos`);
+    let format_time = formatTime(remaining);
+    console.log(`⏱️ Tiempo restante: ${format_time} segundos`);
 
     if (remaining === 0) {
       clearInterval(interval);
-      console.log("✅ ¡Tiempo completado!");
+      console.log("¡Tiempo completado!");
     }
   }, 1000);
 }
